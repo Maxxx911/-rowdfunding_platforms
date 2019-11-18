d534da22bc63bf7816ec2c25f13aa3da79b8eebb
Maxxx911 <maxrikal@gmail.com>
2019-11-18 23:04:13 +0300

    Add seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,42 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+@user = User.new( 
+  login: 'Best',
+  email: 'example@email.com',
+  first_name: 'Maxim',
+  middle_name: 'Middle name',
+  last_name: 'Last name',
+  birthday: Date.today
+)
+@user.password = 'password'
+@user.token = GenerateTokenService.generate(@user)
+@user.save
+
+@categories_id = []
+@categories_id << Category.find_or_create_by(name: 'first').id
+@categories_id << Category.find_or_create_by(name: 'second').id
+@categories_id << Category.find_or_create_by(name: 'third').id
+
+@project = Project.create( 
+  title: 'first',
+  description: 'First description',
+  image_url: 'https://riafan.ru/uploads/2018/05/12/orig-1526128107315bc0def6e8cdb0d4c0bf3b0a0ab23c.jpeg',
+  end_time: Date.today + 15.day,
+  sum_goal: 200,
+  current_sum: 0,
+  owner: @user,
+  category_ids: @categories_id
+)
+
+@project = Project.create( 
+  title: 'Second',
+  description: 'Second description',
+  image_url: 'https://riafan.ru/uploads/2018/05/12/orig-1526128107315bc0def6e8cdb0d4c0bf3b0a0ab23c.jpeg',
+  end_time: Date.today + 15.day,
+  sum_goal: 200,
+  current_sum: 0,
+  owner: @user,
+  category_ids: @categories_id
+)
