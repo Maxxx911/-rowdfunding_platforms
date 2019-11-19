992ba5ad5f8e4678a5be8161883a7c5aef135fd5
Maxxx911 <maxrikal@gmail.com>
2019-11-19 20:12:22 +0300

    Add rubocop yml file
    Fix codestyle
    
    Allow cross origin access

--- app/controllers/api/v1/projects_controller.rb
+++ app/controllers/api/v1/projects_controller.rb
@@ -1,104 +1,106 @@
-class Api::V1::ProjectsController < ApplicationController
-  before_action :project_params, only: %i[create]
-  before_action :project_params_for_update, only: %i[update]
-  before_action :set_project_with_user_token, only: %i[update destroy]
-  before_action :set_project, only: %i[show]
+module Api
+  module V1
+    class ProjectsController < ApplicationController
+      before_action :project_params, only: %i[create]
+      before_action :project_params_for_update, only: %i[update]
+      before_action :set_project_with_user_token, only: %i[update destroy]
+      before_action :set_project, only: %i[show]
 
-  def index
-    render json: { success: true, errors: {}, result: { projects: serialize_resource(find_project_with_filters)} }
-  end
-
-  def create
-    @project = Project.new(title: project_params[:title],
-                           description: project_params[:description],
-                           end_time: project_params[:end_time],
-                           sum_goal: project_params[:sum_goal],
-                           current_sum: project_params[:current_sum],
-                           image_url: project_params[:image_url],
-                           owner: User.find_by(token: project_params[:user_token]),
-                           categories: Category.find(project_params[:categories_id])
-                          )
-    if @project.save
-      render json: { success: true, errors: {}, result: { project: serialize_resource(@project) }}
-    else
-      render json: { success: false, errors: @project.errors.messages, result: {} }
-    end
-  end
-
-  def update
-    if @project
-      if @project.update(project_params_for_update)
-        render json: { success: true, errors: {}, result: { project: serialize_resource(@project) } }
-      else
-        render json: { success: false, errors: @project.errors.messages, result: {} }
+      def index
+        render json: { success: true, errors: {}, result: { projects: serialize_resource(find_project_with_filters) } }
       end
-    else
-      render json: { success: false, errors: { project: "Project with id #{params[:id]} not found"}, result: {} }
-    end
-  end
 
-  def destroy
-    if @project
-      if @project.delete
-        render json: { success: true, errors: {}, result: {} }
-      else
-        render json: { success: false, errors: @project.errors.messages, result: {} }
+      def create
+        @project = Project.new(title: project_params[:title],
+                               description: project_params[:description],
+                               end_time: project_params[:end_time],
+                               sum_goal: project_params[:sum_goal],
+                               current_sum: project_params[:current_sum],
+                               image_url: project_params[:image_url],
+                               owner: User.find_by(token: project_params[:user_token]),
+                               categories: Category.find(project_params[:categories_id])
+                              )
+        if @project.save
+          render json: { success: true, errors: {}, result: { project: serialize_resource(@project) } }
+        else
+          render json: { success: false, errors: @project.errors.messages, result: {} }
+        end
       end
-    else
-      render json: { success: false, errors: { project: "Project with id #{params[:id]} not found"}, result: {} }
-    end
-  end
 
-  def show
-    if @project
-      render json: { success: true, errors: {}, result: { project: serialize_resource(@project) }}
-    else
-      render json: { success: false, errors: { project: "Project with id #{params[:id]} not found"}, result: {} }
-    end
-  end
+      def update
+        if @project
+          if @project.update(project_params_for_update)
+            render json: { success: true, errors: {}, result: { project: serialize_resource(@project) } }
+          else
+            render json: { success: false, errors: @project.errors.messages, result: {} }
+          end
+        else
+          render json: { success: false, errors: { project: "Project with id #{params[:id]} not found" }, result: {} }
+        end
+      end
 
-  private
+      def destroy
+        if @project
+          if @project.delete
+            render json: { success: true, errors: {}, result: {} }
+          else
+            render json: { success: false, errors: @project.errors.messages, result: {} }
+          end
+        else
+          render json: { success: false, errors: { project: "Project with id #{params[:id]} not found" }, result: {} }
+        end
+      end
 
-  def find_project_with_filters
-    ActiveRecord::Base.transaction do
-      @projects = Project.all
-      @projects = @projects.where('title LIKE ?', "%#{params[:title]}%") if params[:title].present?
-      @projects = @projects.where('description LIKE ?', "%#{params[:description]}%") if params[:description].present?
-      @projects = @projects.where('end_time < ?', params[:end_time]) if params[:end_time].present?
-      @projects = find_project_with_category(eval(params[:categories_id])) if params[:categories_id].present?
-    end
-    @projects
-  end
+      def show
+        if @project
+          render json: { success: true, errors: {}, result: { project: serialize_resource(@project) } }
+        else
+          render json: { success: false, errors: { project: "Project with id #{params[:id]} not found" }, result: {} }
+        end
+      end
 
-  def find_project_with_category(category_ids)
-    projects = []
-    @projects.each do |project|
-      if (project.category_ids - (category_ids)).count < project.category_ids.count
-        projects << project
-      else
-        next
+      private
+
+      def find_project_with_filters
+        ActiveRecord::Base.transaction do
+          @projects = Project.all
+          @projects = @projects.where('title LIKE ?', "%#{params[:title]}%") if params[:title].present?
+          @projects = @projects.where('description LIKE ?', "%#{params[:description]}%") if params[:description].present?
+          @projects = @projects.where('end_time < ?', params[:end_time]) if params[:end_time].present?
+          @projects = find_project_with_category(eval(params[:categories_id])) if params[:categories_id].present?
+        end
+        @projects
+      end
+
+      def find_project_with_category(category_ids)
+        projects = []
+        @projects.each do |project|
+          next unless (project.category_ids - category_ids).count < project.category_ids.count
+
+          projects << project
+        end
+        projects
+      end
+
+      def project_params
+        params.permit(:title, :description, :end_time,
+                      :sum_goal, :current_sum, :image_url,
+                      :user_token, categories_id: [])
+      end
+
+      def project_params_for_update
+        params.permit(:title, :description, :end_time,
+                      :sum_goal, :current_sum, :image_url,
+                      categories_id: [])
+      end
+
+      def set_project_with_user_token
+        @project = Project.find_by(id: params[:id], owner: User.find_by(token: params[:user_token]))
+      end
+
+      def set_project
+        @project = Project.find(params[:id])
       end
     end
-    projects
-  end
-
-  def project_params
-    params.permit(:title, :description, :end_time,
-                  :sum_goal, :current_sum, :image_url,
-                  :user_token, categories_id: [])
-  end
-
-  def project_params_for_update
-    params.permit(:title, :description, :end_time,
-                  :sum_goal, :current_sum, :image_url,
-                  categories_id: [])
-  end
-
-  def set_project_with_user_token
-    @project = Project.find_by(id: params[:id], owner: User.find_by(token: params[:user_token]))
-  end
-
-  def set_project
-    @project = Project.find(params[:id])
   end
 end
--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,87 +1,91 @@
-class Api::V1::UsersController < ApplicationController
-  before_action :user_params, only: %i[create sign_in change_password]
-  before_action :user_params_for_update, only: %i[update]
-  before_action :set_user_by_login, only: %i[sign_in]
-  before_action :set_user_by_id, only: %i[change_password update]
+module Api
+  module V1
+    class UsersController < ApplicationController
+      before_action :user_params, only: %i[create sign_in change_password]
+      before_action :user_params_for_update, only: %i[update]
+      before_action :set_user_by_login, only: %i[sign_in]
+      before_action :set_user_by_id, only: %i[change_password update]
 
-  def create
-    @user = User.new(email: user_params[:email],
-                     login: user_params[:login],
-                     birthday: user_params[:birthday],
-                     first_name: user_params[:first_name],
-                     middle_name: user_params[:middle_name],
-                     last_name: user_params[:last_name])
-    @user.password = user_params[:password]
-    if @user.save
-      generate_user_token
-      render json: { success: true, errors: {}, result: { token: @user.token } }
-    else
-      render json: { success: false, errors: @user.errors.messages, result: {} }
-    end
-  end
-
-  def sign_in
-    if @user && valid_password?
-      render json: { success: true, errors: {}, result: { token: @user.token } }
-    else
-      render json: { success: false, errors: { authorization: 'Wrong login or password'}, result: {} }
-    end
-  end
-
-  def change_password
-    if @user && valid_token? && valid_password? && user_params[:confirm_new_password] == user_params[:new_password]
-      @user.password = user_params[:new_password]
-      generate_user_token
-      render json: { success: true, errors: {}, result: { token: @user.token } }
-    else
-      render json: { success: false, errors: { change_password: 'Something went wrong, check the entered data'}, result: {} }
-    end
-  end
-
-  def update
-    if @user
-      if @user.update(user_params_for_update)
-        render json: { success: true, errors: {}, result: { token: @user.token } }
-      else
-        render json: { success: false, errors: @user.errors.messages, result: {} }
+      def create
+        @user = User.new(email: user_params[:email],
+                         login: user_params[:login],
+                         birthday: user_params[:birthday],
+                         first_name: user_params[:first_name],
+                         middle_name: user_params[:middle_name],
+                         last_name: user_params[:last_name])
+        @user.password = user_params[:password]
+        if @user.save
+          generate_user_token
+          render json: { success: true, errors: {}, result: { token: @user.token } }
+        else
+          render json: { success: false, errors: @user.errors.messages, result: {} }
+        end
       end
-    else
-      render json: { success: false, errors: { user: "User with id #{params[:id]} not found"}, result: {} }
+
+      def sign_in
+        if @user && valid_password?
+          render json: { success: true, errors: {}, result: { token: @user.token } }
+        else
+          render json: { success: false, errors: { authorization: 'Wrong login or password'}, result: {} }
+        end
+      end
+
+      def change_password
+        if @user && valid_token? && valid_password? && user_params[:confirm_new_password] == user_params[:new_password]
+          @user.password = user_params[:new_password]
+          generate_user_token
+          render json: { success: true, errors: {}, result: { token: @user.token } }
+        else
+          render json: { success: false, errors: { change_password: 'Something went wrong, check the entered data'}, result: {} }
+        end
+      end
+
+      def update
+        if @user
+          if @user.update(user_params_for_update)
+            render json: { success: true, errors: {}, result: { token: @user.token } }
+          else
+            render json: { success: false, errors: @user.errors.messages, result: {} }
+          end
+        else
+          render json: { success: false, errors: { user: "User with id #{params[:id]} not found"}, result: {} }
+        end
+      end
+
+      private
+
+      def generate_user_token
+        @user.token = GenerateTokenService.generate(@user)
+        @user.save!
+      end
+
+      def valid_token?
+        @user.token == user_params[:token]
+      end
+
+      def valid_password?
+        @user.password == user_params[:password]
+      end
+
+      def user_params
+        params.permit(:login, :email, :password,
+                      :confirm_password, :first_name,
+                      :middle_name, :last_name, :birthday,
+                      :token, :new_password, :confirm_new_password )
+      end
+
+      def user_params_for_update
+        params.permit(:first_name, :middle_name, :last_name,
+                      :email, :birthday)
+      end
+
+      def set_user_by_id
+        @user = User.find(params[:id])
+      end
+
+      def set_user_by_login
+        @user = User.find_by_login(user_params[:login])
+      end
     end
-  end
-  
-  private
-
-  def generate_user_token
-    @user.token = GenerateTokenService.generate(@user)
-    @user.save!
-  end
-
-  def valid_token?
-    @user.token == user_params[:token]
-  end
-
-  def valid_password?
-    @user.password == user_params[:password]
-  end
-
-  def user_params
-    params.permit(:login, :email, :password,
-                  :confirm_password, :first_name,
-                  :middle_name, :last_name, :birthday,
-                  :token, :new_password, :confirm_new_password )
-  end
-
-  def user_params_for_update
-    params.permit(:first_name, :middle_name, :last_name,
-                  :email, :birthday)
-  end
-
-  def set_user_by_id
-    @user = User.find(params[:id])
-  end
-
-  def set_user_by_login
-    @user = User.find_by_login(user_params[:login])
   end
 end
